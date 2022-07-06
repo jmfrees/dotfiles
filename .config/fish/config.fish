a78a24cb8dc4678fd83ad873150efc5b62dace6d
Jonathan Frees <jmfrees@uab.edu>
2022-07-06 14:24:30 -0500

    Move to use onetime.fish for safer tracking of fish configs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,17 +1,10 @@
 alias cp="cp -i"                #  confirm before overwriting something
-alias df='df -h'                # human-readable sizes
-alias du='du -h'                # ditto
 alias free='free -m'            # show sizes in MB
 alias rm='rm -iv'               # confirm before deletion
-alias icat="kitty +kitten icat"
-alias d="kitty +kitten diff"
-alias hg='kitty +kitten hyperlinked_grep'
 alias vim='lvim'
-
 # alias for git directory belonging to my dotfiles
 alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 
-starship init fish | source
+set --export PATH $PATH $HOME/.local/bin
 
-# Created by `pipx` on 2021-05-15 00:36:32
-set PATH $PATH /home/jmfrees/.local/bin
+starship init fish | source
